567088d581fc16da9adaa8376d72e9b08bc26c5c
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2023-03-25 14:35:24 +0800

    feat:新增111年資訊 更新資訊及修改ＪＳ

--- js/slider_bar.js
+++ js/slider_bar.js
@@ -2,34 +2,34 @@
 //slider-bar 設定
 //==================
 // set up an array to hold the months
-var years = ["107", "108", "109", "110"];
+var years = ['107', '108', '109', '110', '111'];
 
-if ($("#slider-bar") !== null) {
+if ($('#slider-bar') !== null) {
 }
-$("#slider-bar")
+$('#slider-bar')
   .slider({
     min: 0,
     max: years.length - 1,
   })
   // add pips with the labels set to "months"
-  .slider("pips", {
-    rest: "label",
+  .slider('pips', {
+    rest: 'label',
     labels: years,
   })
   // and whenever the slider changes, lets echo out the month
-  .on("slidechange", function (e, ui) {
+  .on('slidechange', function (e, ui) {
     let btnValue = ui.value;
     let content;
-    $(".year_content").each(function (e) {
-      let content = JSON.parse(this.dataset.content);
+    $('.year_content').each(function (e) {
+      let content = Number(this.dataset.content);
       if (content === btnValue) {
-        $(this).addClass("year_active");
-        $(this).siblings().removeClass("year_active");
-        $(".ui-slider-handle").html($(this).data("year"));
+        $(this).addClass('year_active');
+        $(this).siblings().removeClass('year_active');
+        $('.ui-slider-handle').html($(this).data('year'));
       } else {
         return;
       }
     });
   });
 //一開始預設 按鈕年份
-$(".ui-slider-handle").html($(".year_active").data("year"));
+$('.ui-slider-handle').html($('.year_active').data('year'));
